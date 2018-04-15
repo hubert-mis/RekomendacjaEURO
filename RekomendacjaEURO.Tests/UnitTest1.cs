e94f72171a15ee9d636397700074ffd89a5f126b
Hubert <hubert.k.mis@gmail.com>
2018-04-15 18:14:39 +0200

    Add project files.

---
+++ RekomendacjaEURO.Tests/UnitTest1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace RekomendacjaEURO.Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
